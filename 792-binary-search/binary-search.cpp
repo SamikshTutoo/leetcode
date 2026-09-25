f72a105692214008d5b3e26134c19f1748912dcc
SamikshTutoo <154685399+SamikshTutoo@users.noreply.github.com>
2026-09-25 22:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (45.53%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int key) {
+         
+         int n=arr.size();
+         int start=0, end=n-1 ,mid;
+         
+
+    while(start<=end)
+    {
+        //Mid ko find karo
+        mid=(start+end)/2;
+        //arr[mid]==key
+        if(arr[mid]==key)
+        return mid;
+        //arr[mid]<key
+        else if(arr[mid]<key)
+        start=mid+1;
+        //arr[mid]>key
+        else
+        end=mid-1;
+
+    }
+    return -1;
+    }
+};
